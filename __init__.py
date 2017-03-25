b02dde585f1b883adf4fc9773fe0cba46465444b
Pakholchuk Nik <pahniki@gmail.com>
2017-03-25 15:46:47 +0300

    init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+from argparsev2 import inputparse
+from parser_cls import Parser_cls
+
+date_dict = {'host_files': '', 'remote_dir': '', 'keys': [], 'username': '', 'ip': '', 'port': '', 'password': ''}
+
+tmp_dict,unknownlist =inputparse()
+date_dict.update(tmp_dict)
+date_dict.update(Parser_cls.main(date_dict,unknownlist))
+print ('final dict')
+print(date_dict)
