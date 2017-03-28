ccafa4fc3321eaa6e1c4e8126a245af50ee3a352
Pakholchuk Nik <pahniki@gmail.com>
2017-03-28 03:28:07 +0300

    grammar

--- rsyncer.py
+++ rsyncer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Useless script just to make a beautiful/userfriendly name to executing our ap.
+# Useless script just to make a beautiful/userfriendly name to execute our ap.
 
 from __init__ import main
 
